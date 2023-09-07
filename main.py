758aee3e2b3cfa09a33aea0f01e4fd194f394516
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-09-07 12:31:05 +0300

    used click to allow a user to be added to the database

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from database.db import Base, engine
+from database.db import Base, engine, Session
 import click
+from models import User
 
-#Define ANSI codes for formatting
+
+# Define ANSI codes for formatting
 class TextStyle:
     BOLD = "\x1b[1m"
     RESET = "\x1b[0m"
@@ -10,16 +12,19 @@ class TextStyle:
     YELLOW = "\x1b[33m"
     CYAN = "\x1b[36m"
 
-#Define a CLI group
+
+# Define a CLI group
 @click.group()
 def cli():
     pass
 
-#Helper function to print a separator
+
+# Helper function to print a separator
 def print_separator():
     print(TextStyle.BOLD + "*" * 70 + TextStyle.RESET)
 
-#Define the main menu command
+
+# Define the main menu command
 @cli.command()
 def menu():
     """Main Menu - Choose an option"""
@@ -43,11 +48,43 @@ def menu():
         if option == "7":
             break
 
-#Helper function to execute chosen options
+# Set to store unique usernames
+unique_usernames = set()
+
+
+# Define the 'add-user' command
+@cli.command()
+@click.option("--username", prompt="Add a username", help="Enter the username")
+def add_user(username):
+    """
+    Add a user to the system
+    """
+    db = Session()
+    existing_user = db.query(User).filter(User.username == username).first()
+
+    if existing_user:
+        db.close()
+        click.echo(f"{TextStyle.RED} {username} already exists.")
+        return
+
+    user = User(username=username)
+    db.add(user)
+    db.commit()
+    db.close()
+    unique_usernames.add(username)
+    click.echo(f"{TextStyle.GREEN} {username} added successfully.")
+
+
+# Helper function to execute chosen options
 def execute_option(option):
     while option not in ["1", "2", "3", "4", "5", "6", "7"]:
-        click.echo("Invalid option. Please choose a valid option.")
+        click.echo(f"{TextStyle.RED}Invalid option. Please choose a valid option." + TextStyle.RESET)
         option = click.prompt("Enter a valid option")
+
+    if option == "1":
+        add_user()  
+    elif option == "7":
+        click.echo(f"{TextStyle.YELLOW} Thank you for choosing this bookstore!" + TextStyle.RESET)
 
 
 cli.add_command(menu)
