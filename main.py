b436396e5839b1dcd3e135c323fd1e6dd67a8126
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-09-07 16:01:37 +0300

    Added a functionality so that a user can borrow and return books.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from database.db import Base, engine, Session
 import click
-from models import User, Book
+from models import User, Book, BorrowedBook, Fines
+import datetime
 
 
 # Define ANSI codes for formatting
@@ -75,26 +76,42 @@ def add_user(username):
     unique_usernames.add(username)
     click.echo(f"{TextStyle.GREEN} {username} added successfully.")
 
-#Dictionary to store book data
+
+# Dictionary to store book data
 books = {}
 
-#Define the 'add-book' command
+
+# Define the 'add-book' command
 @cli.command()
-@click.option("--title", prompt = "Enter the title of the book", help = "Enter the title of the book")
-@click.option("--author", prompt = "Enter the author of the book", help = "Enter the author of the book")
-@click.option("--quantity", prompt = "Enter the number of books", help = "Enter the number of books")
-@click.option("--price", prompt = "Enter the price of one unit item", help = "Enter the price of one unit item")
+@click.option(
+    "--title", prompt="Enter the title of the book", help="Enter the title of the book"
+)
+@click.option(
+    "--author",
+    prompt="Enter the author of the book",
+    help="Enter the author of the book",
+)
+@click.option(
+    "--quantity", prompt="Enter the number of books", help="Enter the number of books"
+)
+@click.option(
+    "--price",
+    prompt="Enter the price of one unit item",
+    help="Enter the price of one unit item",
+)
 def add_book(title, author, quantity, price):
     """Add a book to the system"""
 
     db = Session()
-    book = Book(title = title, author = author, status = True, inventory = quantity, price = price) 
+    book = Book(
+        title=title, author=author, status=True, inventory=quantity, price=price
+    )
 
     db.add(book)
     db.commit()
     db.close()
 
-    #Update the books dictionary with book information
+    # Update the books dictionary with book information
     books[title] = {
         "author": author,
         "status": True,
@@ -104,6 +121,142 @@ def add_book(title, author, quantity, price):
 
     click.echo(f"{TextStyle.GREEN} {title} by {author} has been added successfully.")
 
+# Define the 'borrow-book' command
+@cli.command()
+@click.option("--user_name", prompt="Enter the username", help="Enter the username")
+def borrow_book(user_name):
+    """
+    Borrow a book from the system
+    """
+    db = Session()
+
+    user = db.query(User).filter(User.username == user_name).first()
+
+    if user is None:
+        db.close()
+        click.echo(f"{TextStyle.RED}{user_name} not found." + TextStyle.RESET)
+        return
+
+    # Check if the user already has a book borrowed
+    if db.query(BorrowedBook).filter(BorrowedBook.user_id == user.id).first():
+        db.close()
+        click.echo(
+            f"{TextStyle.RED}{user_name} already has a book borrowed. "
+            " Please return your borrowed book before borrowing another."
+            + TextStyle.RESET
+        )
+        return
+
+    book_title = click.prompt("Enter the book title")
+
+    book = db.query(Book).filter(Book.title == book_title).first()
+
+    if book is None:
+        db.close()
+        click.echo(f"{TextStyle.RED}{book_title} not found." + TextStyle.RESET)
+        return
+
+    # Check if the book is already borrowed
+    if db.query(BorrowedBook).filter(BorrowedBook.book_id == book.id).first():
+        db.close()
+        click.echo(
+            f"{TextStyle.RED}Book '{book_title}' is already borrowed." + TextStyle.RESET
+        )
+        return
+
+    # Calculate the return date as two days from the current date
+    return_date = datetime.datetime.now() + datetime.timedelta(days=2)
+
+    # Create a new BorrowedBook entry with the user's ID, book's ID, and return date
+    borrow_book = BorrowedBook(
+        user_id=user.id, book_id=book.id, return_date=return_date
+    )
+    db.add(borrow_book)
+
+    # Update the book status to False (indicating it's borrowed)
+    book.status = False
+
+    db.commit()
+    db.close()
+
+    click.echo(
+        f"{TextStyle.GREEN}{user_name} has borrowed '{book_title}' successfully."
+        + TextStyle.RESET
+    )
+
+# Define the 'return-book' command
+@cli.command()
+@click.option(
+    "--book_title", prompt="Enter the book title", help="Enter the book title to return"
+)
+def return_book(book_title):
+    """
+    Return a book to the system
+    """
+    db = Session()
+
+    # Find the borrowed book entry based on the book title
+    borrowed_book = (
+        db.query(BorrowedBook).join(Book).filter(Book.title == book_title).first()
+    )
+
+    # Check if the book has not been borrowed
+    if not borrowed_book:
+        # If the book has not been borrowed, close the database session
+        db.close()
+
+        # Display a message indicating that the book was not found in the borrowed books
+        click.echo(
+            f"{TextStyle.RED}Book '{book_title}' not found in the borrowed books."
+            + TextStyle.RESET
+        )
+    else:
+        # If the book has been borrowed, proceed with returning the book
+
+        # Get the return date of the borrowed book and the current date
+        return_date = borrowed_book.return_date
+        current_date = datetime.datetime.now()
+
+        # Check if the book is returned late (current date is greater than return date)
+        if current_date > return_date:
+            # Calculate the number of days late
+            days_late = (current_date - return_date).days
+
+            # Calculate the late fee based on the number of days late
+            late_fee = days_late * 200
+
+            # Display a message indicating that the book is returned late and the late fee
+            click.echo(
+                f"{TextStyle.RED}{book_title} returned late by {days_late} days."
+                f" You are charged Ksh.{late_fee} as a late fee."
+                + TextStyle.RESET
+            )
+
+            # Get the user ID associated with the borrowed book
+            user_id = borrowed_book.user_id
+
+            # Create a fine entry with the user's ID and the calculated late fee
+            fine_entry = Fines(user_id=user_id, amount=late_fee)
+
+            # Add the fine entry to the database
+            db.add(fine_entry)
+        else:
+            # If the book is returned on time, display a success message
+            click.echo(f"{TextStyle.GREEN}{book_title} returned successfully." + TextStyle.RESET)
+
+        # Set the status of the book back to "Available"
+        borrowed_book.book.status = True
+
+        # Delete the borrowed book entry from the database
+        db.delete(borrowed_book)
+        db.commit()
+        db.close()
+
+        # Display a message indicating that the book has been returned successfully
+        click.echo(
+            f"{TextStyle.GREEN}Book '{book_title}' returned successfully."
+            + TextStyle.RESET
+        )
 
 # Helper function to execute chosen options
 def execute_option(option):
@@ -118,6 +271,10 @@ def execute_option(option):
         add_user()
     elif option == "2":
         add_book()
+    elif option == "3":
+        borrow_book()
+    elif option == "4":
+        return_book()
     elif option == "7":
         click.echo(
             f"{TextStyle.YELLOW} Thank you for choosing this bookstore!"
